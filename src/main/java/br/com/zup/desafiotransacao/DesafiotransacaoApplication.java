7ee4113a266b5d13ce904eea063dd9a885d799d1
Pedro <pedro.diniz@zup.com.br>
2021-08-30 10:55:59 -0300

    Primeiro commit, projeto criado

---
+++ src/main/java/br/com/zup/desafiotransacao/DesafiotransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafiotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiotransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/desafiotransacao/DesafiotransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafiotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
